6934a7e049dfe1d32a70cd3ada545620bc7d8efe
pcrandall <crandall.phillip@gmail.com>
2021-01-04 06:38:51 -0600

    font explanation

--- defaultFont.go
+++ defaultFont.go
@@ -1,6 +1,24 @@
 package figlet4go
 
-// Build in source font from starwars.flf
+// Explanation of the .flf file header
+// THE HEADER LINE
+//
+// The header line gives information about the FIGfont.  Here is an example
+// showing the names of all parameters:
+//
+//           flf2a$ 6 5 20 15 3 0 143 229    NOTE: The first five characters in
+//             |  | | | |  |  | |  |   |     the entire file must be "flf2a".
+//            /  /  | | |  |  | |  |   \
+//   Signature  /  /  | |  |  | |   \   Codetag_Count
+//     Hardblank  /  /  |  |  |  \   Full_Layout*
+//          Height  /   |  |   \  Print_Direction
+//          Baseline   /    \   Comment_Lines
+//           Max_Length      Old_Layout*
+//
+//   * The two layout parameters are closely related and fairly complex.
+//       (See "INTERPRETATION OF LAYOUT PARAMETERS".)
+//
+
 const defaultFont = `flf2a& 11 8 18 0 25
 
  /$$       /$$
